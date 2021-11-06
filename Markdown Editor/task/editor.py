b9f474730248d3336c31f67df2f5f93d5737ac3a
rolmos14 <olmos.rafa@gmail.com>
2021-11-06 13:29:25 +0100

    Stage 2/5. Very simple version of Markdown editor, it just accepts commands and shows available commands.

--- Markdown Editor/task/editor.py
+++ Markdown Editor/task/editor.py
@@ -1,10 +1,28 @@
-lennon = ''' # John Lennon
-or ***John Winston Ono Lennon*** was one of *The Beatles*.
-Here are the songs he wrote I like the most:
-* Imagine
-* Norwegian Wood
-* Come Together
-* In My Life
-* ~~Hey Jude~~ (that was *McCartney*)'''
+class Markdown:
 
-print(lennon)
+    def __init__(self):
+        self.formatters = ["plain", "bold", "italic", "header", "link", "inline-code", "ordered-list",
+                           "unordered-list", "new-line"]
+        self.commands = ["!help", "!done"]
+
+    def help(self):
+        print("Available formatters: " + " ".join(self.formatters))
+        print("Special commands: " + " ".join(self.commands))
+
+    def done(self):
+        pass
+
+    def menu(self):
+        while True:
+            command = input("Choose a formatter: ")
+            if command == "!done":
+                self.done()
+                break
+            if command == "!help":
+                self.help()
+            elif command not in self.formatters:
+                print("Unknown formatting type or command")
+
+
+editor = Markdown()
+editor.menu()
